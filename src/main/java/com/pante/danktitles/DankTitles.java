e2cb35f4f1a96e5ca2408809ef0a486f6bda2069
Pante <matthiasngeozhongsim@yahoo.com>
2016-02-22 23:52:30 +0800

    3rd's the charm?
    Removed garbage code at the bottom of DankTitles

--- src/main/java/com/pante/danktitles/DankTitles.java
+++ src/main/java/com/pante/danktitles/DankTitles.java
@@ -5,7 +5,7 @@
  */
 package com.pante.danktitles;
 
-import com.pante.danktitles.commands.*;
+import com.pante.danktitles.utilities.CommandHandler;
 import com.pante.danktitles.utilities.FileHandler;
 import java.io.IOException;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,34 +19,13 @@ public class DankTitles extends JavaPlugin {
     
     @Override
     public void onEnable() {
-
+        FileHandler.initialiseAllFiles();
+        CommandHandler.registerAllCommands();
     }
     @Override
     public void onDisable() {
-        try {
-            FileHandler.titles.save(FileHandler.titlesFile);
-            FileHandler.players.save(FileHandler.playersFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
+        FileHandler.saveAllFiles();
         instance = null;
     }
+    
 }
-
-        
-        getCommand("danktitles").setExecutor(new DankTitles());
-        getCommand("addtitles").setExecutor(new AddTitleCommand());
-        getCommand("removetitles").setExecutor(new RemoveTitleCommand());
-        getCommand("title").setExecutor(new TitleCommand());
-        
-        try {
-            FileHandler.infoFile = FileHandler.initialiseFile(FileHandler.info,"info.txt");
-            FileHandler.titlesFile = FileHandler.initialiseFile(FileHandler.titles,"titles.yml");
-            FileHandler.playersFile = FileHandler.initialiseFile(FileHandler.players,"players.yml");
-            
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            getLogger().severe("Failed to retrieve or create files ");
-            getServer().getPluginManager().disablePlugin(this);
-          }
